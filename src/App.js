994f883e909d0d1fed5cef430401576c84eeaf3d
adunolami <adunolaadenike20@gmail.com>
2024-07-16 21:07:19 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import { Home } from './Pages/Home';
+import About from './Pages/About';
+import { HomeAdmin } from './Admin/HomeAdmin';
+import { AboutAdmin } from './Admin/AboutAdmin';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
+export const URI = process.env.
+REACT_APP_SERVER_URI
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+          {/* <HomePage/> */}
+        <Routes>
+            <Route path="/"  element={<Home/>}/>
+            <Route path="/about"  element={<About/>}/>
+            <Route path="/homeAdmin"  element={<HomeAdmin/>}/>
+            <Route path="/aboutAdmin"  element={<AboutAdmin/>}/>
+            
+      </Routes>
+      </Router>
+      <ToastContainer/>
+    </div>
+  );
+}
+
+export default App;
